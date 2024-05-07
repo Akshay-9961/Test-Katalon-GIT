1b4d1067575151db17fc1b9e9bcedb86c30640ba
Akshay-9961 <akshay.sakhare9961@gmail.com>
2024-05-07 19:17:00 +0530

    Added "/"

--- Scripts/Login/Script1715088880449.groovy
+++ Scripts/Login/Script1715088880449.groovy
@@ -17,5 +17,5 @@ import com.kms.katalon.core.windows.keyword.WindowsBuiltinKeywords as Windows
 import internal.GlobalVariable as GlobalVariable
 import org.openqa.selenium.Keys as Keys
 
-WebUI.openBrowser('https://app-simplymusic.growexx.com\\')
+WebUI.openBrowser('https://app-simplymusic.growexx.com/')
 
